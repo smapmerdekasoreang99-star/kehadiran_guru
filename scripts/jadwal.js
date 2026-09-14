31142ac1bee617ee70fa4f35b09188baa98c15a7
smapmerdekasoreang99-star <smapmerdekasoreang99@gmail.com>
2026-09-14 21:46:50 +0700

    Update jadwal.js

--- scripts/jadwal.js
+++ scripts/jadwal.js
@@ -59,7 +59,7 @@ async function boot() {
     if (isSupabaseConfigured) {
         const [{ data: guru }, { data: kelas }, { data: mapel }, { data: jam }] =
             await Promise.all([
-                supabaseClient.from("kg_guru").select("id, nama").order("nama"),
+                supabaseClient.from("v_guru_aktif").select("id, nama").order("nama"),
                 supabaseClient.from("kg_kelas").select("id, nama_kelas, tingkat"),
                 supabaseClient.from("kg_mapel").select("id, nama_mapel").order("nama_mapel"),
                 supabaseClient.from("kg_jam_pelajaran").select("*").order("jam_ke"),
